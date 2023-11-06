24de061646b42fd8974f457033c7746b8eab48f1
Scott Ueland <41088895+bnhf@users.noreply.github.com>
2023-11-06 02:29:28 -0700

    Added movie support to manual recordings

--- scripts/manualrecordings.sh
+++ scripts/manualrecordings.sh
@@ -11,6 +11,7 @@ genres=$(echo "\"$7"\" | sed 's/,/","/g' | sed 's/" /"/g')
   [[ $genres == "\"none"\" ]] && genres=""
 image="$8"
 source="manual"
+type="$9"
 
 recordingJob() {
 cat <<EOF
@@ -34,6 +35,32 @@ cat <<EOF
 EOF
 }
 
-recordingJSON=$(echo -n "$(recordingJob)" | tr -d '\n')
+recordingJobMovie() {
+cat <<EOF
+{
+    "Name": "$name",
+    "Time": $time,
+    "Duration": $duration,
+    "Channels": ["$channel"],
+    "Airing": {
+        "Source": "$source",
+        "Channel": "$channel",
+        "Time": $time,
+        "Duration": $duration,
+        "Title": "$name",
+        "Summary": "$summary",
+        "Genres": [$genres],
+        "MovieID": "$source/$channel",
+        "Image": "$image",
+        "Categories": ["Movie"]
+    }
+}
+EOF
+}
+
+[[ "$type" == "tv" ]] \
+  && recordingJSON=$(echo -n "$(recordingJob)" | tr -d '\n')
+[[ "$type" == "movie" ]] \
+  && recordingJSON=$(echo -n "$(recordingJobMovie)" | tr -d '\n')
 
 curl -v --data-binary "$recordingJSON" http://$dvr/dvr/jobs/new
