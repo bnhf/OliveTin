60256f1f02f364335e357881afc2b86a3ca22a9e
Scott Ueland <41088895+bnhf@users.noreply.github.com>
2024-07-22 07:34:32 -0600

    Update logalerter.sh

--- scripts/logalerter.sh
+++ scripts/logalerter.sh
@@ -37,7 +37,7 @@ logAlerts() {
       echo -e "The following CDVR ($dvr) log lines match your filters:\n"
       cat $logFile
       } | msmtp -a default $ALERT_EMAIL_TO
-      rm $logFile
+      > $logFile
     fi
 
     sleep $runInterval
